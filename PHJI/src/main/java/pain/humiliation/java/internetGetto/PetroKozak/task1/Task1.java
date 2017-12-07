7734f0cf9118a490d1a29d5ee8a0950b42334efa
PetroKozak <kozak.petya@gmail.com>
2017-12-07 20:55:34 +0200

    Correct first commit. Stupid idiot wrote a method in the foulder TEST

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/PetroKozak/task1/Task1.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/PetroKozak/task1/Task1.java
@@ -1,4 +1,9 @@
 package pain.humiliation.java.internetGetto.PetroKozak.task1;
 
 public class Task1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+    }
 }
--- PHJI/src/test/java/pain/humiliation/java/internetGetto/PetroKozak/Task1.java
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/PetroKozak/Task1.java
@@ -1,10 +1,8 @@
 package pain.humiliation.java.internetGetto.PetroKozak;
 
 public class Task1 {
-    public static void main(String[] args){
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.print(i + " ");
-        }
-    }
+
+
+
 }
