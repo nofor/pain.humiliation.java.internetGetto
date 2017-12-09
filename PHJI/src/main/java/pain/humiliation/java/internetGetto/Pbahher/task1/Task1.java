5a90d89f39b0a61982b441cec967f7aee105ebba
Peter Bahher <pbahher@gmail.com>
2017-12-09 11:08:56 +0200

    task1

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task1/Task1.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task1/Task1.java
@@ -1,4 +1,10 @@
 package pain.humiliation.java.internetGetto.Pbahher.task1;
 
 public class Task1 {
+
+    public static void main (String args[]) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
 }
