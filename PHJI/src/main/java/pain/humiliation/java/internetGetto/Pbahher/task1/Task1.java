09324c93bb06fe53f9eb31016d21119016235ee5
Peter Bahher <pbahher@gmail.com>
2017-12-10 20:05:33 +0200

    Code conventions

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task1/Task1.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task1/Task1.java
@@ -4,39 +4,40 @@ public class Task1 {
 
     public void firstTask() {
         for (int i = 0; i <= 10; i++) {
+
             System.out.print(i + " ");
         }
     }
 
-    public void delenieOstatok() {
+    public void divisionRestNumber() {
         int q = 21;
         int w = 8;
 
-        System.out.println("21 / 8 = " + q / w + ", Остаток " + q % w);
+        System.out.println("21 / 8 = " + q/w + ", rest " + q%w);
     }
 
-    public void summaDvuhChisel() {
+    public void sumOfTwoNumbers() {
         int a = 24;
-        int b = a % 10;
-        int c = (a / 10) % 10;
+        int b = a%10;
+        int c = (a/10)%10;
 
-        System.out.println("Сумма цифр числа " + a + " равна " + (b + c));
+        System.out.println("Sum of numbers " + a + " is " + (b+c));
     }
 
-    public void okruglenie() {
+    public void roundingOfNumber() {
         double n = 2.8;
         int c = (int) (0.5 + n);
 
         System.out.println(c);
     }
 
-    public void summaTrehChisel() {
+    public void sumOfThreeNumbers() {
         int a = 243;
-        int b = a % 10;
-        int c = (a / 10) % 10;
-        int d = ((a / 10) / 10) % 10;
+        int b = a%10;
+        int c = (a/10)%10;
+        int d = ((a/10)/10)%10;
 
-        System.out.println("Сумма цифр числа " + a + " равна " + (b + c + d));
+        System.out.println("Sum of numbers " + a + " is " + (b+c+d));
     }
 
 }
--- PHJI/src/test/java/pain/humiliation/java/internetGetto/Pbahher/Task1Test.java
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/Pbahher/Task1Test.java
@@ -8,18 +8,18 @@ public class Task1Test {
     static Task1 task1 = new Task1();
 
     @Test
-    public void FirstTask() { task1.FirstTask(); }
+    public void firstTask() { task1.firstTask(); }
 
     @Test
-    public void DelenieOstatok() { task1.DelenieOstatok(); }
+    public void divisionRestNumber() { task1.divisionRestNumber(); }
 
     @Test
-    public void SummaDvuhChisel() { task1.SummaDvuhChisel(); }
+    public void sumOfTwoNumbers() { task1.sumOfTwoNumbers(); }
 
     @Test
-    public void Okruglenie() { task1.Okruglenie(); }
+    public void roundingOfNumber() { task1.roundingOfNumber(); }
 
     @Test
-    public void SummaTrehChisel() { task1.SummaTrehChisel(); }
+    public void sumOfThreeNumbers() { task1.sumOfThreeNumbers(); }
 
 }
