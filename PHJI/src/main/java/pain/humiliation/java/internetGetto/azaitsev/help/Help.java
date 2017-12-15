2826dfe361ae0c31f02517f9b04a984c2fef09eb
azaitsev <azaitsev@productengine.com>
2017-12-15 17:14:29 +0200

    Custom string reverse

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/azaitsev/help/Help.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/azaitsev/help/Help.java
@@ -1,4 +1,21 @@
 package pain.humiliation.java.internetGetto.azaitsev.help;
 
 public class Help {
+
+    /**
+     * Method which reverse string.
+     * @param a - incoming string
+     * @return - reverse incoming string
+     */
+    public String reverseString(String a) {
+        char[] mass = a.toCharArray();
+        char[] reverse = new char[mass.length];
+
+        for (int i = mass.length - 1, j = 0; i >= 0; i--, j++) {
+            reverse[j] = mass[i];
+        }
+
+        return new String(reverse);
+    }
+
 }
