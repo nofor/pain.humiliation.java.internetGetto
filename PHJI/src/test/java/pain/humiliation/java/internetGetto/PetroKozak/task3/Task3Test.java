62ccdcf2b71ece32859324683b7ecedb231ba973
PetroKozak <kozak.petya@gmail.com>
2017-12-17 11:20:52 +0200

    Commit all TODOs done + created packages in TEST

---
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/PetroKozak/task3/Task3Test.java
@@ -0,0 +1,10 @@
+package pain.humiliation.java.internetGetto.PetroKozak.task3;
+
+import org.junit.Test;
+
+public class Task3Test {
+    Task3 task3 = new Task3();
+
+    @Test
+    public void testTask3(){ task3.mainString("Life","is","bitch"); }
+}
