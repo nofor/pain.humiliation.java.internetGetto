bd46f3b62b4c50a348240a485d9a4f2c22db67ab
calllka <qfcfirf@gmail.com>
2017-12-19 11:56:59 +0200

    Code fix. Add custom method for reverse String (sorry Zaicev). Change variables name

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/Calllka/task4/Task4.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Calllka/task4/Task4.java
@@ -10,47 +10,45 @@ package pain.humiliation.java.internetGetto.Calllka.task4;
 8. Don't merge (bad word). At next lessons will merge together.**/
 
 public class Task4 {
-    private String[] mainMassive = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
 
-
-    public void runMethod(int x){
-        outputMassive(mainMassive);
-        outputReverseMassive(mainMassive);
-        deleteIndex(mainMassive, x);
+    public void runMethod(String[] inputMassive, int index){
+        outputMassive(inputMassive);
+        outputReverseMassive(inputMassive);
+        deleteIndex(inputMassive, index);
     }
 
-    private void outputMassive (String[] mass){
-        String temp = "";
+    private void outputMassive (String[] inputMassive){
+        String outputSentence = "";
 
-        for (String str : mass){
-            temp = temp + str;
+        for (String str : inputMassive){
+            outputSentence = outputSentence + str;
         }
 
-        System.out.println(temp);
-
-    }
-
-    private void outputReverseMassive (String[] mass){
-        String temp = "";
-
-        for (int i = mass.length - 1; i >= 0; i--){
-            temp = temp + mass[i];
-        }
-
-        System.out.println(temp);
+        System.out.println(outputSentence);
 
     }
 
-    private void deleteIndex(String[] mass, int x){
-        String temp = "";
+    private void outputReverseMassive (String[] inputMassive){
+        String outputSentence = "";
 
-        for (int i = 0; i < mass.length; i++){
-            if(i == x) mass[i] = "_";
-
-            temp = temp + mass[i];
+        for (int i = inputMassive.length - 1; i >= 0; i--){
+            outputSentence = outputSentence + inputMassive[i];
         }
 
-        System.out.println(temp);
+        System.out.println(outputSentence);
+
+    }
+
+    private void deleteIndex(String[] inputMassive, int x){
+        String outputSentence = "";
+
+        for (int i = 0; i < inputMassive.length; i++){
+            if(i == x) inputMassive[i] = "_";
+
+            outputSentence = outputSentence + inputMassive[i];
+        }
+
+        System.out.println(outputSentence);
 
     }
 }
