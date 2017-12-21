9e9db089305a75431535e0100ed4eca44f17bfc0
Peter Bahher <pbahher@gmail.com>
2017-12-21 22:38:07 +0200

    task5 hometask1

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task5/Task5.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task5/Task5.java
@@ -4,4 +4,77 @@ package pain.humiliation.java.internetGetto.Pbahher.task5;
  * Created by pbahher on 12/21/17.
  */
 public class Task5 {
+
+    public void homework1() {
+        int[] a = {2, 3, 0, 4, 0, 3, 0, 0};
+        int z = 0;
+        int count = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == z) {
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+
+    public void homework2() {
+
+    }
+
+    public void homework3() {
+
+    }
+
+    public void homework4() {
+
+    }
+
+    public void homework5() {
+
+    }
+
+    public void homework6() {
+
+    }
+
+    public void homework7() {
+
+    }
+
+    public void homework8() {
+
+    }
+
+    public void homework9() {
+
+    }
+
+    public void homework10() {
+
+    }
+
+    public void homework11() {
+
+    }
+
+    public void homework12() {
+
+    }
+
+    public void homework13() {
+
+    }
+
+    public void homework14() {
+
+    }
+
+    public void homework15() {
+
+    }
+
+    public void homework16() {
+
+    }
 }
