5a3ce6704b13cdcf6c634849078cc800b7a28848
BP-azaits002 <sA4efAph>
2017-12-21 21:43:46 +0200

    todo for Alexey

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task4/Task4.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task4/Task4.java
@@ -1,13 +1,13 @@
 package pain.humiliation.java.internetGetto.Pbahher.task4;
 
-import javax.xml.bind.SchemaOutputResolver;
+import javax.xml.bind.SchemaOutputResolver;  //todo remove unusefull import
 
 /**
  * Created by pbahher on 12/21/17.
  */
 public class Task4 {
 
-    public void runIterationMethod(String [] incomingArray) {  //todo Add index for deleting
+    public void runIterationMethod(String [] incomingArray) {  //todo 1. Add index for deleting 2. Use this method for all 3 methods from above list
         arrayItteration(incomingArray);
 
 
@@ -33,9 +33,9 @@ public class Task4 {
         System.out.println(a);
     }
 
-    public void indexArrayDelete (String [] incomingArray) {
+    public void indexArrayDelete (String [] incomingArray) {  //todo 1. make it dinamic.
         String a = "";
-        int b = 11;
+        int b = 11;  //todo remove it, we need dinamic index for deletion
 
         for (int i = 0; i <= incomingArray.length; i++) {
             if (i == b)
--- PHJI/src/test/java/pain/humiliation/java/internetGetto/Pbahher/task4/Task4Test.java
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/Pbahher/task4/Task4Test.java
@@ -1,7 +1,7 @@
 package pain.humiliation.java.internetGetto.Pbahher.task4;
 
 import org.junit.Test;
-import pain.humiliation.java.internetGetto.Pbahher.task4.Task4;
+import pain.humiliation.java.internetGetto.Pbahher.task4.Task4;  //todo remove unusefull inport
 
 /**
  * Created by pbahher on 12/21/17.
@@ -14,7 +14,7 @@ public class Task4Test {
     public void stringMainMethod() {
         String[] a = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
 
-        task4.runIterationMethod(a);
+        task4.runIterationMethod(a);  //todo Use only main method here
         task4.reverseArray(a);
         task4.indexArrayDelete(a);
     }
