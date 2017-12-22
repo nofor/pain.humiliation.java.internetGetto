ee64f5d8ec971d40a9a983ca9a13a5d5a77bc12d
Peter Bahher <pbahher@gmail.com>
2017-12-22 15:30:47 +0200

    homework

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task5/Task5.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task5/Task5.java
@@ -1,5 +1,9 @@
 package pain.humiliation.java.internetGetto.Pbahher.task5;
 
+import com.sun.org.apache.bcel.internal.classfile.FieldOrMethod;
+
+import javax.xml.bind.SchemaOutputResolver;
+
 /**
  * Created by pbahher on 12/21/17.
  */
@@ -7,11 +11,11 @@ public class Task5 {
 
     public void homework1() {
         int[] a = {2, 3, 0, 4, 0, 3, 0, 0};
-        int z = 0;
+        int b = 0;
         int count = 0;
 
         for (int i = 0; i < a.length; i++) {
-            if (a[i] == z) {
+            if (a[i] == b) {
                 count++;
             }
         }
@@ -19,23 +23,78 @@ public class Task5 {
     }
 
     public void homework2() {
+        int[] a = {2, 3, 0, 4, 0, 3, 0, 0};
+        int b = a[0];
 
+        for (int i = 0; i < a.length; i++) {
+            if (b < a[i]) {
+                b = a[i];
+            }
+        }
+        System.out.println(b);
     }
 
     public void homework3() {
+        int[] a = {2, 7, 0, 4, 0, 3, 0, 0};
+        int x = 3;
 
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == x) {
+                System.out.println("First element of occurrence " + i );
+                break;
+            }
+        }
     }
 
     public void homework4() {
+        int[] a = {1, 3, 7, 0, 4, 7, 0, 0};
+        int b = a[0];
+        int count = 0;
 
+        for (int i = 0; i < a.length; i++) {
+            if (b <= a[i]) {
+                b = a[i];
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 
     public void homework5() {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+        for (int i = a.length -1; i >= 0; i--) {
+            System.out.print(a[i]);
+        }
     }
 
     public void homework6() {
+        int[] a = {9, 4, 6, 5, 3, 7, 8, 1, 2};
 
+        for (int i = 0; i < a.length; i++) {
+            int min = a[i];
+            int min_i = i;
+
+            for (int j = i; j < a.length; j++) {
+                if (a[j] < min) {
+                    min = a[j];
+                    min_i = j;
+                }
+            }
+            if (i != min_i) {
+                int temp = a[i];
+                a[i] = a[min_i];
+                a[min_i] = temp;
+            }
+        }
+
+        String stringAsArray = "";
+
+        for (int i : a) {
+            stringAsArray = stringAsArray + i + " ";
+        }
+
+        System.out.println(stringAsArray);
     }
 
     public void homework7() {
