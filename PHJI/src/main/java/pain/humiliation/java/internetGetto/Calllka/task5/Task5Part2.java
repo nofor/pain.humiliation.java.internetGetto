c4800cfb9d287130c91a5f94827c4a6f9f7481bf
calllka <qfcfirf@gmail.com>
2017-12-26 18:39:35 +0200

    Add Task5Part2 and Task5Part2Test with completed homework 11-14.

---
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Calllka/task5/Task5Part2.java
@@ -0,0 +1,101 @@
+package pain.humiliation.java.internetGetto.Calllka.task5;
+
+public class Task5Part2 {
+
+    public void homework11() {
+        double[] firstMassiveDouble = new double[9];
+        double[] secondMassiveDouble = new double[9];
+        double[] resultMassive = new double[firstMassiveDouble.length];
+        System.out.println();
+
+        for (int i = 0; i < firstMassiveDouble.length; i++) {
+            firstMassiveDouble[i] = Math.round((Math.random() * 9));
+            secondMassiveDouble[i] = Math.round((Math.random() * 9));
+            resultMassive[i] = firstMassiveDouble[i] - secondMassiveDouble[i];
+        }
+
+        for (double temp : resultMassive) {
+            System.out.print(temp + " ");
+        }
+    }
+
+    public void homework12() {
+        int[] firstMassiveDouble = new int[9];
+        int[] secondMassiveDouble = new int[9];
+        int[] resultMassive = new int[firstMassiveDouble.length + secondMassiveDouble.length];
+        System.out.println("Homework 12");
+
+        for (int i = 0; i < firstMassiveDouble.length; i++) {
+            firstMassiveDouble[i] = (int) Math.round((Math.random() * 9));
+            secondMassiveDouble[i] = (int) Math.round((Math.random() * 9));
+            resultMassive[i] = firstMassiveDouble[i];
+            resultMassive[firstMassiveDouble.length + i] = secondMassiveDouble[i];
+        }
+
+        for (int temp : resultMassive) {
+            System.out.print(temp + " ");
+        }
+    }
+
+    public void homework13() {
+        int[] firstMassiveDouble = new int[9];
+        int[] secondMassiveDouble = new int[9];
+        int[] resultMassive;
+        int resultMassiveLength = 0, count = 0;
+        System.out.println("Homework 13");
+
+        for (int i = 0; i < firstMassiveDouble.length; i++) {
+            firstMassiveDouble[i] = (int) Math.round((Math.random() * 9));
+            secondMassiveDouble[i] = (int) Math.round((Math.random() * 9));
+
+            if(firstMassiveDouble[i] == secondMassiveDouble[i]){
+                count++;
+            }
+        }
+
+        resultMassive = new int[count];
+
+        for (int i = 0; i < firstMassiveDouble.length; i++){
+            if(firstMassiveDouble[i] == secondMassiveDouble[i]){
+                resultMassive[resultMassiveLength] = firstMassiveDouble[i];
+                resultMassiveLength++;
+            }
+        }
+
+        for (int temp : resultMassive) {
+            System.out.print(temp + " ");
+        }
+    }
+
+    public void homework14() {
+        int[] firstMassiveDouble = new int[9];
+        int[] secondMassiveDouble = new int[9];
+        int[] resultMassive;
+        int resultMassiveLength = 0, count = 0;
+        System.out.println("Homework 14");
+
+        for (int i = 0; i < firstMassiveDouble.length; i++) {
+            firstMassiveDouble[i] = (int) Math.round((Math.random() * 9));
+            secondMassiveDouble[i] = (int) Math.round((Math.random() * 9));
+
+            if(firstMassiveDouble[i] != secondMassiveDouble[i]){
+                count = count + 2;
+            }
+        }
+
+        resultMassive = new int[count];
+
+        for (int i = 0; i < firstMassiveDouble.length; i++){
+            if(firstMassiveDouble[i] != secondMassiveDouble[i]){
+                resultMassive[resultMassiveLength] = firstMassiveDouble[i];
+                resultMassive[resultMassiveLength + 1] = secondMassiveDouble[i];
+                resultMassiveLength = resultMassiveLength + 2;
+            }
+        }
+
+        for (int temp : resultMassive) {
+            System.out.print(temp + " ");
+        }
+    }
+
+}
---
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/Calllka/task5/Task5Part2Test.java
@@ -0,0 +1,16 @@
+package pain.humiliation.java.internetGetto.Calllka.task5;
+
+import org.junit.Test;
+
+public class Task5Part2Test {
+    private Task5Part2 task5 = new Task5Part2();
+
+    @Test
+    public void homeworkTest(){
+        task5.homework11();
+        task5.homework12();
+        task5.homework13();
+        task5.homework14();
+    }
+
+}
