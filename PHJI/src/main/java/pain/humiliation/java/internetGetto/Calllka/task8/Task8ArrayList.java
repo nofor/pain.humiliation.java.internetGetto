ec8699724d754f92aa55c92be25cc610e330e551
Calllka <qfcfirf@gmail.com>
2018-01-23 21:08:07 +0200

    Add my ArrayList class. Part 1

---
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Calllka/task8/Task8ArrayList.java
@@ -0,0 +1,217 @@
+package pain.humiliation.java.internetGetto.Calllka.task8;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class Task8ArrayList implements List, RandomAccess, Cloneable, Serializable {
+    private static int MAIN_CAPACITY = 2;
+    private static Object[] mainArray = new Object[MAIN_CAPACITY];
+    private int size = 0;
+    private boolean flag;
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i++) {
+            if (mainArray[i].equals(o)) {
+                flag = true;
+                break;
+            } else {
+                flag = false;
+            }
+        }
+
+        return flag;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] tempArray = new Object[size()];
+        for (int i = 0; i < size(); i++) {
+            tempArray[i] = mainArray[i];
+        }
+        return tempArray;
+    }
+
+    @Override
+    public boolean add(Object o) {
+
+        try {
+            mainArray[size] = o;
+            flag = true;
+        } catch (IndexOutOfBoundsException ex) {
+            increaseArrayCapacity(mainArray, size * 2);
+            mainArray[size] = o;
+
+            flag = true;
+        }
+
+        size++;
+        return flag;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (contains(o)) {
+            for (int i = 0; i < size(); i++) {
+                if (mainArray[i].equals(o)) {
+                    for (int j = i; j < size() - 1; j++) {
+                        mainArray[j] = mainArray[j + 1];
+                    }
+                    mainArray[size--] = null;
+
+                    break;
+                }
+            }
+
+            flag = true;
+        } else {
+            flag = false;
+        }
+
+        return flag;
+    }
+
+    @Override
+    public boolean addAll(Collection collection) {
+        int sumSizeArray = size + collection.size();
+        Object[] collectionArray = collection.toArray();
+
+        increaseArrayCapacity(mainArray, sumSizeArray);
+
+        for (int i = 0; i < collection.size(); i++) {
+            mainArray[size++] = collectionArray[i];
+            flag = true;
+        }
+
+        size = mainArray.length;
+
+        return flag;
+    }
+
+    @Override
+    public boolean addAll(int i, Collection collection) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size(); i++) {
+            mainArray[i] = null;
+        }
+
+        size = 0;
+    }
+
+    @Override
+    public Object get(int i) {
+        return mainArray[i];
+    }
+
+    @Override
+    public Object set(int i, Object o) {
+        for(int position = 0; position < size(); position++){
+            if (i == position){
+                mainArray[position] = o;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void add(int i, Object o) {
+        Object[] some = increaseArrayCapacity(mainArray, size + 1);
+
+        for(int position = 0; position < size(); position++){
+            if (position == i) {
+                for (int j = position; j < size(); j++){
+
+                    Object temp = mainArray[j];
+                    mainArray[j] = some[j + 1];
+                    some[j + 1]= temp;
+                    some[i] = o;
+                }
+            }
+        }
+
+        mainArray = some;
+
+    }
+
+    @Override
+    public Object remove(int i) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int i) {
+        return null;
+    }
+
+    @Override
+    public List subList(int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection collection) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] objects) {
+        return new Object[0];
+    }
+
+    private Object[] increaseArrayCapacity(Object[] arrayForIncrease, int increaseSize) {
+        Object[] tempArray = new Object[increaseSize];
+
+        for (int i = 0; i < size(); i++) {
+            tempArray[i] = arrayForIncrease[i];
+        }
+
+        mainArray = tempArray;
+
+        return mainArray;
+    }
+}
