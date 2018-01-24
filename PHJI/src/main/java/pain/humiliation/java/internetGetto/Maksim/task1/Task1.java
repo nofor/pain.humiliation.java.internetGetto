197aae4dcf88b555cb79d93c4ae74a1f39594f35
Maksim <masya.k98@gmail.com>
2018-01-24 22:39:19 +0200

    Output numbers on screen in Task1.

---
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Maksim/task1/Task1.java
@@ -0,0 +1,10 @@
+package pain.humiliation.java.internetGetto.Maksim.task1;
+
+public class Task1 {
+
+    public void numbersOutput(){
+        for(int i = 0; i <= 10; i++ ){
+            System.out.println(i);
+        }
+    }
+}
