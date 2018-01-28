4dff9cc5123d682387061e70a955b9ac6d765599
PetroKozak <kozak.petya@gmail.com>
2018-01-28 13:16:31 +0200

    Deleted unseful packages

--- PHJI/src/test/java/pain/humiliation/java/internetGetto/PetroKozak/CollectionTest/CollectionTest.java
+++
@@ -1,7 +0,0 @@
-package pain.humiliation.java.internetGetto.PetroKozak.CollectionTest;
-
-import pain.humiliation.java.internetGetto.PetroKozak.collectiontask.MyCollection;
-
-public class CollectionTest {
-
-}
