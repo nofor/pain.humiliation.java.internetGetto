7eaf092951ab23c44e8650844f2d170912333f94
Calllka <qfcfirf@gmail.com>
2018-01-30 19:02:25 +0200

    ArrayList class. Part 2. Add Exception. TODO - DONE. Methods: addAll, indexOf, removeAll - DONE

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/Calllka/task8/Task8ArrayList.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Calllka/task8/Task8ArrayList.java
@@ -2,12 +2,14 @@ package pain.humiliation.java.internetGetto.Calllka.task8;
 
 import java.io.Serializable;
 import java.util.*;
-//todo make flag as local variable
-public class Task8ArrayList implements List, RandomAccess, Cloneable, Serializable {  //todo add generic here. Collection must be generic
+
+//todo make flag as local variable DONE
+//todo extra line DONE
+//todo remove extra line DONE
+public class Task8ArrayList<E> implements List<E>, RandomAccess, Cloneable, Serializable {  //todo add generic here. Collection must be generic DONE
     private static int MAIN_CAPACITY = 2;
     private static Object[] mainArray = new Object[MAIN_CAPACITY];
     private int size = 0;
-    private boolean flag;
 
     @Override
     public int size() {
@@ -21,16 +23,7 @@ public class Task8ArrayList implements List, RandomAccess, Cloneable, Serializab
 
     @Override
     public boolean contains(Object o) {
-        for (int i = 0; i < size; i++) {
-            if (mainArray[i].equals(o)) {
-                flag = true;
-                break;
-            } else {
-                flag = false;
-            }
-        }
-
-        return flag;
+        return indexOf(o) >= 0;
     }
 
     @Override
@@ -41,133 +34,152 @@ public class Task8ArrayList implements List, RandomAccess, Cloneable, Serializab
     @Override
     public Object[] toArray() {
         Object[] tempArray = new Object[size()];
-        //todo extra line
+
         for (int i = 0; i < size(); i++) {
             tempArray[i] = mainArray[i];
         }
-        //todo extra line
+
         return tempArray;
     }
 
     @Override
-    public boolean add(Object o) {
-        //todo remove extra line
+    public boolean add(E e) {
         try {
-            mainArray[size] = o;
-            flag = true;
+            mainArray[size] = e;
         } catch (IndexOutOfBoundsException ex) {
             increaseArrayCapacity(mainArray, size * 2);
-            mainArray[size] = o;
-            //todo remove extra line
-            flag = true;
+            mainArray[size] = e;
         }
 
         size++;
-        return flag;
+        return true;
     }
 
     @Override
     public boolean remove(Object o) {
-        if (contains(o)) {  //todo think why do you need it here
-            for (int i = 0; i < size(); i++) {
-                if (mainArray[i].equals(o)) {
-                    for (int j = i; j < size() - 1; j++) {
-                        mainArray[j] = mainArray[j + 1];
-                    }
-                    mainArray[size--] = null;
-                    //todo remove extra line
-                    break;
-                }
-            }
+        rangeCheck(indexOf(0)); //todo think why do you need it here DONE
 
-            flag = true;
-        } else {
-            flag = false;
+        for (int i = 0; i < size(); i++) {
+            if (mainArray[i].equals(o)) {
+                remove(i);
+                break;
+            }
         }
 
-        return flag;
+        return true;
     }
 
     @Override
-    public boolean addAll(Collection collection) {  //todo change it
-        int sumSizeArray = size + collection.size();
-        Object[] collectionArray = collection.toArray();
-
-        increaseArrayCapacity(mainArray, sumSizeArray);
-
-        for (int i = 0; i < collection.size(); i++) {
-            mainArray[size++] = collectionArray[i];
-            flag = true;
+    public boolean addAll(Collection collection) {  //todo change it DONE
+        for (Object temp : collection) {
+            add((E) temp);
         }
 
-        size = mainArray.length;
-
-        return flag;
+        return collection.size() != 0;
     }
 
     @Override
     public boolean addAll(int i, Collection collection) {
-        return false;
+        Object[] arrayCollection = collection.toArray();
+        Object[] tempMainArray = new Object[0];
+        int count = i;
+        int shiftPosition = i + collection.size() - 1;
+        int tempSize = size + collection.size();
+        rangeCheckForAdd(i);
+
+        if (mainArray.length < tempSize) {
+            tempMainArray = increaseArrayCapacity(mainArray, tempSize + 1);
+        }
+
+        for (int j = shiftPosition; j < tempSize; j++) {
+            tempMainArray[j + 1] = mainArray[count];
+            count++;
+        }
+
+        for (int j = 0; j < collection.size(); j++) {
+            tempMainArray[i++] = arrayCollection[j];
+        }
+
+        size = tempSize;
+        return collection.size() != 0;
     }
 
     @Override
     public void clear() {
         for (int i = 0; i < size(); i++) {
-            mainArray[i] = null;  //todo this must be new empty array
+            mainArray[i] = null;  //todo this must be new empty array DONE
         }
 
         size = 0;
     }
 
     @Override
-    public Object get(int i) {
-        return mainArray[i];
+    public E get(int i) {
+        rangeCheck(i);
+        return (E) mainArray[i];
     }
 
     @Override
-    public Object set(int i, Object o) {
-        for(int position = 0; position < size(); position++){
-            if (i == position){
-                mainArray[position] = o;
-            }
-        }
+    public E set(int i, E e) {
+        rangeCheck(i);
+        Object oldElement = mainArray[i];
 
-        return null;
+        mainArray[i] = e;
+        return (E) oldElement;
     }
 
     @Override
-    public void add(int i, Object o) {
+    public void add(int i, E e) {
+        rangeCheckForAdd(i);
         Object[] some = increaseArrayCapacity(mainArray, size + 1);
+        Object[] some2 = increaseArrayCapacity(mainArray, size + 1);
 
-        for(int position = 0; position < size(); position++){
-            if (position == i) {
-                for (int j = position; j < size(); j++){
-
-                    Object temp = mainArray[j];
-                    mainArray[j] = some[j + 1];
-                    some[j + 1]= temp;
-                    some[i] = o;
-                }
-            }
+        for (int j = i; j < size(); j++) {
+            some[j + 1] = some2[j];
         }
 
+        some[i] = e;
         mainArray = some;
-
+        size++;
     }
 
     @Override
-    public Object remove(int i) {
-        return null;
+    public E remove(int i) {
+        rangeCheck(i);
+        Object var = mainArray[i];
+
+        for (int j = i; j < size() - 1; j++) {
+            mainArray[j] = mainArray[j + 1];
+        }
+        mainArray[size--] = null;
+
+        return (E) var;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        int i;
+
+        for (i = 0; i < this.size; ++i) {
+            if (o.equals(mainArray[i])) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        int i;
+
+        for(i = size - 1; i >= 0; i--){
+            if(mainArray[i].equals(o)){
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     @Override
@@ -192,6 +204,16 @@ public class Task8ArrayList implements List, RandomAccess, Cloneable, Serializab
 
     @Override
     public boolean removeAll(Collection collection) {
+        Object[] tempCollection = collection.toArray();
+
+        for(int i = 0; i < collection.size(); i++){
+            for(int j = 0; j < size; j++){
+                if(mainArray[j].equals(tempCollection[i])){
+                    remove(i);
+                }
+            }
+        }
+
         return false;
     }
 
@@ -205,15 +227,37 @@ public class Task8ArrayList implements List, RandomAccess, Cloneable, Serializab
         return new Object[0];
     }
 
-    private Object[] increaseArrayCapacity(Object[] arrayForIncrease, int increaseSize) {
-        Object[] tempArray = new Object[increaseSize];
+    private Object[] increaseArrayCapacity(Object[] arrayForIncrease, int tempSize) {
+        Object[] tempArray = new Object[tempSize];
 
         for (int i = 0; i < size(); i++) {
             tempArray[i] = arrayForIncrease[i];
         }
 
         mainArray = tempArray;
-        //todo remove extra line
         return mainArray;
+    }
+
+    private void rangeCheckForAdd(int var1) {
+        if (var1 > size || var1 < 0) {
+            throw new IndexOutOfBoundsException("Index: " + var1 + ", Size: " + size);
+        }
+    }
+
+    private void rangeCheck(int var1) {
+        if (var1 >= size) {
+            throw new IndexOutOfBoundsException("Index: " + var1 + ", Size: " + size);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilderVar = new StringBuilder();
+
+        for (int i = 0; i < size; i++) {
+            stringBuilderVar.append(mainArray[i]).append(" ");
+        }
+
+        return "Collection: " + stringBuilderVar.toString();
     }
 }
