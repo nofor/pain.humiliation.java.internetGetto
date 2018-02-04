8cb874138ab96c0c2b3890d70633537409792d0d
Peter Bahher <pbahher@gmail.com>
2018-02-04 18:02:21 +0200

    some changes task8

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task8/MyArray.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Pbahher/task8/MyArray.java
@@ -7,18 +7,17 @@ import java.util.*;
  */
 public class MyArray<E> implements List<E>, RandomAccess, Cloneable, java.io.Serializable {
 
-    public E[] values;
-    public static Object[] myArray;
-    public int size = 0;
-    private static final int def_cap = 10;
+    private E[] starArray;
+    private int size = 0;
+    private int def_cap = 10;
 
     public MyArray() {
-        values = (E[]) new Object[def_cap];
+        starArray = (E[]) new Object[def_cap];
     }
 
     @Override
     public int size() {
-        return values.length;
+        return size;
     }
 
     @Override
@@ -46,7 +45,7 @@ public class MyArray<E> implements List<E>, RandomAccess, Cloneable, java.io.Ser
 
     @Override
     public Iterator<E> iterator() {
-        return new ArrayIterator<>(values);
+        return new ArrayIterator<>(starArray);
     }
 
     @Override
@@ -64,10 +63,10 @@ public class MyArray<E> implements List<E>, RandomAccess, Cloneable, java.io.Ser
         boolean flag = true;
 
         try {
-            E[] temp = values;
-            values = (E[]) new Object[temp.length + 1];
-            System.arraycopy(temp, 0, values, 0, temp.length);
-            values[values.length - 1] = e;
+            E[] temp = starArray;
+            starArray = (E[]) new Object[temp.length + 1];
+            System.arraycopy(temp, 0, starArray, 0, temp.length);
+            starArray[starArray.length - 1] = e;
         } catch (ClassCastException ex) {
             ex.printStackTrace();
             flag = false;
@@ -81,7 +80,7 @@ public class MyArray<E> implements List<E>, RandomAccess, Cloneable, java.io.Ser
         range(indexOf(0));
 
         for (int i = 0; i > size; i++) {
-            if (myArray[i].equals(o)) {
+            if (starArray[i].equals(o)) {
                 remove(i);
                 break;
             }
@@ -134,7 +133,7 @@ public class MyArray<E> implements List<E>, RandomAccess, Cloneable, java.io.Ser
     @Override
     public void clear() {
         int fromIndex = 0;
-        int toIndex = values.length - 1;
+        int toIndex = starArray.length - 1;
         ListIterator<E> it = listIterator(fromIndex);
         for (int i = 0, n = toIndex-fromIndex; i<n; i++) {
             it.next();
@@ -145,51 +144,51 @@ public class MyArray<E> implements List<E>, RandomAccess, Cloneable, java.io.Ser
     @Override
     public E get(int index) {
         range(index);
-        return values[index];
+        return starArray[index];
     }
 
     @Override
     public E set(int index, E value) {
         range(index);
-        Object first = myArray[index];
-        myArray[index] = value;
+        Object first = starArray[index];
+        starArray[index] = value;
         return (E) first;
     }
 
     @Override
     public void add(int index, E element) {
-        if (index == values.length){
-            E[] newArray = (E[]) new Integer[values.length * 2];
-            System.arraycopy(values, 0, newArray, 0, index - 1);
-            values = newArray;
+        if (index == starArray.length){
+            E[] newArray = (E[]) new Integer[starArray.length * 2];
+            System.arraycopy(starArray, 0, newArray, 0, index - 1);
+            starArray = newArray;
         }
-        values[index] = element;
+        starArray[index] = element;
         index++;
         size++;
     }
 
     @Override
     public E remove(int index) {
-            E[] temp = values;
-            values = (E[]) new Object[temp.length - 1];
-            System.arraycopy(temp, 0, values, 0, index);
+            E[] temp = starArray;
+            starArray = (E[]) new Object[temp.length - 1];
+            System.arraycopy(temp, 0, starArray, 0, index);
 
             int amountElementAfterIndex = temp.length - index - 1;
 
-            System.arraycopy(temp, index + 1, values, index, amountElementAfterIndex);
+            System.arraycopy(temp, index + 1, starArray, index, amountElementAfterIndex);
 
-        return values[index];
+        return starArray[index];
     }
 
     @Override
     public int indexOf(Object o) {
         if (o == null) {
             for (int i = 0; i < size; i++)
-                if (values[i]==null)
+                if (starArray[i]==null)
                     return i;
         } else {
             for (int i = 0; i < size; i++)
-                if (o.equals(values[i]))
+                if (o.equals(starArray[i]))
                     return i;
         }
 
@@ -200,16 +199,15 @@ public class MyArray<E> implements List<E>, RandomAccess, Cloneable, java.io.Ser
     public int lastIndexOf(Object o) {
         if (o == null) {
             for (int i = size - 1; i >= 0; i--)
-                if (values[i] == null)
+                if (starArray[i] == null)
                     return i;
         } else {
             for (int i = size - 1; i >= 0; i--)
-                if (o.equals(values[i]))
+                if (o.equals(starArray[i]))
                     return i;
         }
         return -1;
     }
-
 
     @Override
     public ListIterator<E> listIterator() {
@@ -223,7 +221,6 @@ public class MyArray<E> implements List<E>, RandomAccess, Cloneable, java.io.Ser
 
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
-        subListRange(fromIndex,toIndex,size);
         return null;
     }
 
@@ -233,23 +230,30 @@ public class MyArray<E> implements List<E>, RandomAccess, Cloneable, java.io.Ser
         }
     }
 
-    public void rangeForAdd(int index){
-        if (index > size || index < 0)
-            throw new IndexOutOfBoundsException(outOfBoundMsg(index));
-    }
-
     private String outOfBoundMsg(int index){
         return "Index: " +index+ ", Size: " +size;
     }
 
-    static void subListRange (int fromIndex, int toIndex, int size) {
-        if (fromIndex < 0)
-            throw new IndexOutOfBoundsException("fromIndex: " +fromIndex);
-        if (toIndex > size)
-            throw new IndexOutOfBoundsException("toIndex: " +toIndex);
-        if (fromIndex > toIndex)
-            throw new IllegalArgumentException("fromIndex(" +fromIndex + ") > toIndex(" +
-                                                                                toIndex + ")");
-    }
+    public class ArrayIterator<E> extends MyArray implements Iterator {
+        public int index;
+        public E[] values;
 
+        ArrayIterator(E[] values) {
+            this.values = values;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index < values.length;
+        }
+
+        @Override
+        public E next() {
+            return values[index++];
+        }
+
+        @Override
+        public void remove() {
+        }
+    }
 }
---
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/Pbahher/task8/Task8Test.java
@@ -0,0 +1,166 @@
+package pain.humiliation.java.internetGetto.Pbahher.task8;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+
+/**
+ * Created by pbahher on 2/4/18.
+ */
+public class Task8Test {
+
+    MyArray<Object> collection = new MyArray<>();
+    MyArray<Object> collection2 = new MyArray<>();
+
+
+    @Before
+    public void beforeTest() {
+        Collections.addAll(collection, "first", "second", "three", "four", "four", "five", "six");
+        Collections.addAll(collection2, "1", "2", "3", "4");
+    }
+
+    @After
+    public void afterTest() {
+        collection.clear();
+        collection2.clear();
+    }
+
+    @Test
+    public void testSize() {
+        System.out.println(collection.size());
+    }
+
+    @Test
+    public void testIsEmpty() {
+        System.out.println(collection.isEmpty());
+    }
+
+    @Test
+    public void testContains() {
+        System.out.println(collection.contains("first"));
+    }
+
+    @Test
+    public void testIterator() {
+        Iterator<Object> it = collection.iterator();
+
+        it.next();
+        it.remove();
+
+        while (it.hasNext()) {
+            System.out.println(it.next()+ " ");
+        }
+    }
+
+    @Test
+    public void testToArray() {
+        Object[] toto = collection.toArray();
+
+        for (Object to : toto) {
+            System.out.println(to + " ");
+        }
+    }
+
+    @Test
+    public void testToArrayT() {
+
+    }
+
+    @Test
+    public void testAdd() {
+        collection.add(3,"seven");
+        System.out.println(collection);
+    }
+
+    @Test
+    public void testRemove() {
+        collection.remove("tree");
+        System.out.println(collection);
+    }
+
+    @Test
+    public void testContainsAll() {
+        System.out.println(collection.containsAll(collection2));
+
+    }
+
+    @Test
+    public void testAddAll() {
+
+    }
+
+    @Test
+    public void AddAllIndx() {
+
+    }
+
+    @Test
+    public  void testRemoveAll() {
+
+    }
+
+    @Test
+    public void testRetainAll() {
+        collection.retainAll(collection2);
+        System.out.println(collection);
+    }
+
+    @Test
+    public void testClear() {
+        collection.clear();
+        System.out.println(collection.size());
+    }
+
+    @Test
+    public void testGet() {
+        System.out.println(collection.get(2));
+    }
+
+    @Test
+    public void testSet() {
+        System.out.println(collection.set(1, "new"));
+
+    }
+
+    @Test
+    public void testAddIndx() {
+
+    }
+
+    @Test
+    public void testRemoveIndx() {
+        collection.remove(2);
+        System.out.println(collection);
+    }
+
+    @Test
+    public void testIndexOff() {
+        System.out.println(collection.indexOf("four"));
+
+    }
+
+    @Test
+    public void testLastIndexOff() {
+        System.out.println(collection.lastIndexOf("four"));
+    }
+
+    @Test
+    public void testListIterator() {
+
+    }
+
+    @Test
+    public void testListIteratorIndx() {
+
+    }
+
+    @Test
+    public void testSubList() {
+
+    }
+}
