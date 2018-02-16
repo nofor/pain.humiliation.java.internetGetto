1ae70f89a3332cbc2f96722a0b73561cca0f8da3
Calllka <qfcfirf@gmail.com>
2018-02-16 11:13:16 +0200

    Some visual changes

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/Calllka/task9/CalllkaTask9.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/Calllka/task9/CalllkaTask9.java
@@ -1,37 +1,43 @@
 package pain.humiliation.java.internetGetto.Calllka.task9;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CalllkaTask9 {
     private ArrayList<Integer> list = new ArrayList<>();
+    private ExecutorService executorService = Executors.newFixedThreadPool(25);
 
-    public void outputElement(ArrayList<Integer> someList){
-        for (int i = 1; i < 26; i++){
-            System.out.println("Cycle number: " + i);
-            for(Integer temp : someList){
-                System.out.println("Cycle number: " + i + " ; " + "Element: " + temp);
-            }
-        }
-    }
-
-    public static synchronized void outputPoolThread(ArrayList<Integer> someList){
-        for(Integer temp : someList){
-            System.out.println("Element: " + temp);
-        }
-    }
-
-    public ArrayList<Integer> addElement(){
-        for (int i = 1; i < 101; i++){
+    public ArrayList<Integer> addElement() {
+        for (int i = 1; i < 101; i++) {
             list.add(i);
         }
 
         return list;
     }
 
-    public static class InnerCalllkaClass implements Runnable{
-        private ArrayList innerCalllkaClassList;
+    public void outputElement(ArrayList<Integer> someList) {
+        for (int i = 1; i < 26; i++) {
+            System.out.println("Cycle number: " + i);
 
-        public InnerCalllkaClass(ArrayList innerCalllkaClassList){
+            for (Integer temp : someList) {
+                System.out.println("Cycle number: " + i + " ; " + "Element: " + temp);
+            }
+        }
+    }
+
+    public void outputElementWithThread(ArrayList<Integer> someList) {
+        for (int i = 0; i < 25; i++) {
+            executorService.execute(new CalllkaTask9.InnerCalllkaClass(someList));
+        }
+
+        executorService.shutdown();
+    }
+
+    private static class InnerCalllkaClass implements Runnable {
+        private ArrayList<Integer> innerCalllkaClassList;
+
+        InnerCalllkaClass(ArrayList<Integer> innerCalllkaClassList) {
             this.innerCalllkaClassList = innerCalllkaClassList;
         }
 
@@ -42,4 +48,9 @@ public class CalllkaTask9 {
         }
     }
 
+    private static void outputPoolThread(ArrayList<Integer> someList) {
+        for (Integer temp : someList) {
+            System.out.println("Element: " + temp);
+        }
+    }
 }
--- PHJI/src/test/java/pain/humiliation/java/internetGetto/Calllka/task9/CalllkaTask9Test.java
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/Calllka/task9/CalllkaTask9Test.java
@@ -1,16 +1,15 @@
 package pain.humiliation.java.internetGetto.Calllka.task9;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class CalllkaTask9Test {
     private CalllkaTask9 task9 = new CalllkaTask9();
     private ArrayList<Integer> simpleArrayList = new ArrayList<>();
-    private ExecutorService executorService = Executors.newFixedThreadPool(25);
+    private long timerArrayList, timeThreadArrayList;
 
     @Before
     public void addMethodTest(){
@@ -18,16 +17,20 @@ public class CalllkaTask9Test {
     }
 
     @Test
-    public void output(){
+    public void outputElementTest(){
+        timerArrayList = System.nanoTime();
         task9.outputElement(simpleArrayList);
+        timerArrayList = System.nanoTime() - timerArrayList;
+
+        timeThreadArrayList = System.nanoTime();
+        task9.outputElementWithThread(simpleArrayList);
+        timeThreadArrayList = System.nanoTime() - timeThreadArrayList;
     }
 
-    @Test
-    public void outputExecutorService(){
-        for (int i = 0; i < 25; i++){
-            executorService.submit(new CalllkaTask9.InnerCalllkaClass(simpleArrayList));
-        }
-
-        executorService.shutdown();
+    @After
+    public void outputResult(){
+        System.out.println("Result timerArrayList: " + timerArrayList );
+        System.out.println("Result timeThreadArrayList: " + timeThreadArrayList );
+        System.out.println("Result timerArrayList-timeThreadArrayList: " + (timerArrayList - timeThreadArrayList));
     }
 }
