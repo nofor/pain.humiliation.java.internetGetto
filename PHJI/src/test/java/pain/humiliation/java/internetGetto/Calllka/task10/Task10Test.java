5dc7ed53ca0733bd490fbb564cbc2907ece5a59a
Calllka <qfcfirf@gmail.com>
2018-02-23 00:33:06 +0200

    Task10_1. First try

--- PHJI/src/test/java/pain/humiliation/java/internetGetto/Calllka/task10/Task10Test.java
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/Calllka/task10/Task10Test.java
@@ -11,11 +11,17 @@ public class Task10Test {
             "/java/pain/humiliation/java/internetGetto/Calllka" +
             "/task10/sourcePackage";
     private Task10 task10 = new Task10(path, fileName);
+    private Task10_1 task10_1 = new Task10_1(path, fileName);
 
     @Test
     public void runOutputFile(){
         task10.outputFileWithScanner();
         task10.outputFileWithBufferReader();
+    }
+
+    @Test
+    public void ere(){
+        task10_1.writeInToHashMap(path, fileName + "2");
     }
 
 }
