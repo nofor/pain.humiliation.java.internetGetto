56d28254e2361940ac1ffa024d46495f970938be
Calllka <qfcfirf@gmail.com>
2018-02-22 22:56:13 +0200

    Rework method for calculate difference time. Complete all TODO

--- PHJI/src/test/java/pain/humiliation/java/internetGetto/Calllka/task10/Task10Test.java
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/Calllka/task10/Task10Test.java
@@ -2,19 +2,20 @@ package pain.humiliation.java.internetGetto.Calllka.task10;
 
 import org.junit.Test;
 
-import java.io.IOException;
 
 public class Task10Test {
 
-    private Task10 task10 = new Task10();
+    private String fileName = "temp.txt";
     private String path = "/home/calllka/Programming/Kurs/" +
             "pain.humiliation.java.internetGetto/PHJI/src/main" +
             "/java/pain/humiliation/java/internetGetto/Calllka" +
-            "/task10/sourcePackage/temp.txt";
+            "/task10/sourcePackage";
+    private Task10 task10 = new Task10(path, fileName);
 
     @Test
-    public void runOutputFile() throws IOException {
-        task10.outputFile(path);
+    public void runOutputFile(){
+        task10.outputFileWithScanner();
+        task10.outputFileWithBufferReader();
     }
 
 }
