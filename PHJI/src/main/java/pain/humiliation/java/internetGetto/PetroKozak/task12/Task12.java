26d525b0bb3574388ea5675745aabcae350dd6b3
PetroKozak <kozak.petya@gmail.com>
2018-03-04 20:35:51 +0200

    made task with exceptions

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/PetroKozak/task12/Task12.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/PetroKozak/task12/Task12.java
@@ -11,9 +11,9 @@ public class Task12 {
         return number * callStackOverFlow(number);
     }
 
-    public void tryHowExceptionsAreWorking(int[] array) {
+    public void handleIndexOutOfBoundsException(int[] array) {
         try {
-            array[8] = 1;
+            array[array.length] = 1;
         } catch (IndexOutOfBoundsException ex) {
             System.out.println("It should be the FUCKING EXCEPTION here. It isn't here, only because I caught IT.\n");
         } finally {
@@ -22,15 +22,97 @@ public class Task12 {
     }
 
     public void usingMyExceptionInAgeValidator(int age) throws PedroException {
-        if (age <= 0) {
+        if (age <= 18) {
             throw new PedroException();
         } else {
             System.out.println("Your age is accepted.");
         }
     }
 
+    public void handleArrayIndexOutOfBoundsException(int[] temp, int x) {
+        try {
+            temp[temp.length] = x;
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("You're out Of Bounds");
+        } finally {
+            int count = 0;
+
+            for (int h : temp) {
+                if (h != 0) {
+                    count++;
+                }
+            }
+
+            temp[count] = x;
+        }
+    }
+
+    public void handleArithmeticException(int a, int b) {
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException ex) {
+            System.out.println("You are not allowed to make this action");
+        }
+    }
+
+    public void handleClassCastException(Object j) {
+        try {
+            System.out.println((String) j);
+        } catch (ClassCastException ex) {
+            System.out.println("You cant cast " + j.getClass().getName() + " to String");
+        } finally {
+            System.out.println("The programm casted it to Integer. Value is: " + j);
+        }
+    }
+
+    public void handleArrayStoreException(Object[] x, Object y) {
+        try {
+            x[1] = y;
+        } catch (ArrayStoreException ex) {
+            System.out.println("You cant add such data type to the " + x.getClass().getName() + " array");
+        }
+    }
+
+    public void handleIllegalArgumentException(int x) {
+        try {
+            setYourAge(x);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("You are to small to watch this Sasha Greys movie. TURN TV OFF!!!");
+        }
+    }
+
+    public void handleNegativeArraySizeException(int size) {
+        try {
+            int[] temp = new int[size];
+        } catch (NegativeArraySizeException ex) {
+            System.out.println("ARRAYS SIZE CANT BE NULL. But your brain size can.");
+        }
+    }
+
+    public void handleNullPointerException(String x) {
+        try {
+            System.out.println(x.equals("something"));
+        } catch (NullPointerException ex) {
+            System.out.println("NullPointerException caught.");
+        }
+    }
+
+    private void setYourAge(int yourAge) {
+        if (yourAge < 18) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void handleNumberFormatException(int[] x) {
+        try {
+            System.out.println(x[new Integer(null)]);
+        } catch (NumberFormatException ex) {
+            System.out.println("You use the wrong format. Please change it.");
+        }
+    }
+
     class PedroException extends Exception {
-        private String message = "The Age Cant Be NULL or Less Than NULL!!";
+        private String message = "YOU SHOULD BE AT LEAST 18 YEARS OLD, YOU LITTLE BASTARD!!";
 
         public PedroException() {
             System.out.println(message);
--- PHJI/src/test/java/pain/humiliation/java/internetGetto/PetroKozak/task12/Task12Test.java
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/PetroKozak/task12/Task12Test.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 public class Task12Test {
     private Task12 task12 = new Task12();
     private int[] array = new int[5];
-    private int age = 0;
+    private Object [] arrayObject = new Integer[5];
+    private int age = 5;
+    private String stringForNullPointer = null;
 
     @Test
     public void testingOutOfMemoryError() {
@@ -18,12 +20,56 @@ public class Task12Test {
     }
 
     @Test
-    public void testHowExceptionWorks() {
-        task12.tryHowExceptionsAreWorking(array);
+    public void testIndexOutOfBoundsException() {
+        task12.handleIndexOutOfBoundsException(array);
     }
 
     @Test
     public void testOwnException() throws Task12.PedroException {
         task12.usingMyExceptionInAgeValidator(age);
     }
+
+    @Test
+    public void testArrayIndexOutOfBoundsException() {
+        task12.handleArrayIndexOutOfBoundsException(array, 2);
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+
+    @Test
+    public void testArithmeticException() {
+        task12.handleArithmeticException(5,0);
+    }
+
+    @Test
+    public void testClassCastException() {
+        task12.handleClassCastException(age);
+    }
+
+    @Test
+    public void testArrayStoreException() {
+        arrayObject [0] = 1;
+        task12.handleArrayStoreException(arrayObject, new String("4"));
+    }
+
+    @Test
+    public void testIllegalArgumentException(){
+        task12.handleIllegalArgumentException(age);
+    }
+
+    @Test
+    public void testNegativeArraySizeException(){
+        task12.handleNegativeArraySizeException(-10);
+    }
+    @Test
+    public void testNullPointerException(){
+        task12.handleNullPointerException(stringForNullPointer);
+    }
+    @Test
+    public void testNumberFormatException(){
+        task12.handleNumberFormatException(array);
+    }
+
 }
