a4775c076bf9ce6f7708fd582d49a6144b2b48fa
PetroKozak <kozak.petya@gmail.com>
2018-03-04 14:06:49 +0200

    Made task12. Tried how exception works, created my own one, called Stack Overflow and OutOfMemory errors.

--- PHJI/src/test/java/pain/humiliation/java/internetGetto/PetroKozak/task12/Task12Test.java
+++ PHJI/src/test/java/pain/humiliation/java/internetGetto/PetroKozak/task12/Task12Test.java
@@ -1,4 +1,29 @@
 package pain.humiliation.java.internetGetto.PetroKozak.task12;
 
+import org.junit.Test;
+
 public class Task12Test {
+    private Task12 task12 = new Task12();
+    private int[] array = new int[5];
+    private int age = 0;
+
+    @Test
+    public void testingOutOfMemoryError() {
+        task12.callOutOfMemoryError();
+    }
+
+    @Test
+    public void testingStackOverFlow() {
+        task12.callStackOverFlow(1);
+    }
+
+    @Test
+    public void testHowExceptionWorks() {
+        task12.tryHowExceptionsAreWorking(array);
+    }
+
+    @Test
+    public void testOwnException() throws Task12.PedroException {
+        task12.usingMyExceptionInAgeValidator(age);
+    }
 }
