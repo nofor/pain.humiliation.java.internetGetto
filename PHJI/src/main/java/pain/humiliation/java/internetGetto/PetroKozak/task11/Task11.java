a243aad32531903499c217fa516354748a0929d0
PetroKozak <kozak.petya@gmail.com>
2018-03-04 13:11:45 +0200

    Added task11 with done TODO's and sorting the map not only by values but also by the keys

--- PHJI/src/main/java/pain/humiliation/java/internetGetto/PetroKozak/task11/Task11.java
+++ PHJI/src/main/java/pain/humiliation/java/internetGetto/PetroKozak/task11/Task11.java
@@ -6,49 +6,48 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Task11 {
-    //todo add extra line
-    public HashMap<String, ArrayList<String>> readFile(String path) throws IOException {    //todo add pattern incoming param and use it under getOnlyStrings() method
+
+    public TreeMap<String, ArrayList<String>> readFile(String path,String regex) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
-        String line;    //todo grup primitive to primitive, other to other
         StringBuilder sb = new StringBuilder();
-        HashMap<String, ArrayList<String>> result = new HashMap<>();
+        TreeMap<String, ArrayList<String>> result = new TreeMap<>();
+        String line;
 
         while ((line = bufferedReader.readLine()) != null) {
             sb.append(line);
             sb.append(" ");
         }
 
-        String[] words = getOnlyStrings(sb.toString().toLowerCase()).split("\\s+");  //todo do not create extra variable
-
-        for (String str : words) {
+        for (String str : getOnlyStrings(sb.toString().toLowerCase(),regex).split("\\s+")) {
             if (result.get(str) != null) {
                 result.get(str).add("|");
             } else {
-                result.put(str, new ArrayList<String>());    //todo <String> is not necessary here
+                result.put(str, new ArrayList<>());
                 result.get(str).add("|");
             }
         }
 
         bufferedReader.close();
-        //todo add extra line
+
         return result;
     }
 
-    public void writeResultInANewFile(HashMap<String, ArrayList<String>> map, File file) throws IOException {
+    public void writeResultInANewFile(TreeMap<String, ArrayList<String>> map, File file) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
         Map<String, ArrayList<String>> sortedMap = sortByArraySizeDesc(map);
+        ArrayList<String> value;
 
         for (String key : sortedMap.keySet()) {
-            ArrayList<String> value = sortedMap.get(key);  //todo initialize it at the beginning
+            value = sortedMap.get(key);
 
-            bufferedWriter.write(key + myToString(value));
+            bufferedWriter.write(key + formatListForWritingIntoTheFile(value));
             bufferedWriter.newLine();
         }
 
         bufferedWriter.close();
     }
 
-    public String myToString(ArrayList x) {    //todo rename it
+    public String formatListForWritingIntoTheFile(ArrayList x) {
         String result = "";
 
         for (Object aX : x) {
@@ -58,22 +57,21 @@ public class Task11 {
         return result;
     }
 
-    public static String getOnlyStrings(String s) {
-        Pattern pattern = Pattern.compile("[^a-z A-Z 0-9]");    //todo make patter as incoming param
+    public static String getOnlyStrings(String s, String regex) {
+        Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(s);
 
         return matcher.replaceAll(" ");
     }
 
     private static Map<String, ArrayList<String>> sortByArraySizeDesc(Map<String, ArrayList<String>> map) {
-        List<ArrayList<String>> list = new LinkedList(map.entrySet());
+        List<Map.Entry<String,ArrayList<String>>> list = new LinkedList(map.entrySet());
 
         Collections.sort(list, (Comparator) Task11::compare);
 
-        Map res = new LinkedHashMap();  //todo move it to beginning
+        Map res = new LinkedHashMap();
 
-        for (Iterator it = list.iterator(); it.hasNext(); ) {    //todo rework it like _for (Map.Entry<String, String> entry : map.entrySet())_
-            Map.Entry entry = (Map.Entry) it.next();
+        for (Map.Entry<String, ArrayList<String>> entry:list ) {
             res.put(entry.getKey(), entry.getValue());
         }
 
@@ -88,10 +86,10 @@ public class Task11 {
         } else if (o2 == null) {
             return -1;
         }
-        //todo add extra line
+
         int size1 = ((List) ((Map.Entry) (o1)).getValue()).size();
         int size2 = ((List) ((Map.Entry) (o2)).getValue()).size();
-        //todo add extra line
+
         return size2 - size1;
     }
 }
